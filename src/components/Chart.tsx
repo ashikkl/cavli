319bf5d6870f289e3593e0a0b14264e0dfa1018d
Ashik K L <ashikkl19112001@gmail.com>
2023-12-08 20:21:06 +0530

    chart works

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -130,6 +130,7 @@ export default function Chart({
         yScale={{
           type: "linear",
         }}
+        
       />
     </div>
   );
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -64,7 +64,7 @@ export default function Landing() {
           </Button>
         </span>
         <Chart
-          data={ formattedData}
+          data={ []}
         />
       </div>
     </header>
